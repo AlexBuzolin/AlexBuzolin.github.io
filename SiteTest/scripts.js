ec191a0edd27017e17cfc8c0d2e6de5aef0008f6
Alex <51996809+AlexBuzolin@users.noreply.github.com>
2019-07-24 23:17:51 +0500

    Add files via upload

--- SiteTest/scripts.js
+++ SiteTest/scripts.js
@@ -15,7 +15,7 @@ function tabbarHorizontalOpnCls() {
         tabbarBtnIcon.style.transform = 'rotateZ(0)';
         tabbarOrderBtn.style.display = 'none';
         tabbarInfoBtn.style.display = 'none';
-        tabbarInfoPanel.style.height = '180px';
+        tabbarInfoPanel.style.height = '140px';
 
         closeTabbarHorizontal = true;
     } else if (closeTabbarHorizontal === true) {
@@ -31,52 +31,10 @@ function tabbarHorizontalOpnCls() {
     }
 }
 
-//Open/Close Vertical Tabbar
-let closeTabbarVertical = false;
-function tabbarVerticalOpnCls() {
-    let tabbarVertical = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical');
-        tabbarVerticalHead = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_head');
-        tabbarRightBtn = document.querySelector('.tabbar_GFBqnm64amWoxqfX_right_btn');
-        tabbarRightBtnIcon = document.querySelector('.tabbar_GFBqnm64amWoxqfX_right_btn_icon');
-        tabbarVerticalInfoPanel = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_info_panel');
-        tabbarVerticalFormsPanel = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_forms_panel');
-        tabbarPosBtns = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_pos_btns');
-        tabbarClosePanel = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_close_panel_btns');
-        tabbarVerticalAuthor = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_author');
-    
-    if (closeTabbarVertical === false) {
-        tabbarVertical.classList.remove('tabbar_GFBqnm64amWoxqfX_vertical_close');
-        tabbarVerticalHead.style.left = '0'
-        tabbarRightBtn.style.left = '320px';
-        tabbarRightBtnIcon.style.transform = 'rotateZ(180deg)';
-        tabbarVerticalInfoPanel.style.left = '0';
-        tabbarVerticalFormsPanel.style.left = '0';
-        tabbarPosBtns.style.display = 'flex';
-        tabbarClosePanel.style.display = 'none';
-        tabbarVerticalAuthor.style.left = '0';
-
-        closeTabbarVertical = true;
-    } else if (closeTabbarVertical = true) {
-        tabbarVertical.classList.add('tabbar_GFBqnm64amWoxqfX_vertical_close');
-        tabbarVerticalHead.style.left = '-320px'
-        tabbarRightBtn.style.left = '120px';
-        tabbarRightBtnIcon.style.transform = 'rotateZ(0)';
-        tabbarVerticalInfoPanel.style.left = '-320px';
-        tabbarVerticalFormsPanel.style.left = '-320px';
-        tabbarPosBtns.style.display = 'none';
-        tabbarClosePanel.style.display = 'flex';
-        tabbarVerticalAuthor.style.left = '-320px';
-
-        closeTabbarVertical = false;
-    }
-}
-
 //Close Tabbar info panel
 function closeInfoPanel() {
     let tabbarInfoPanel = document.querySelector('.tabbar_GFBqnm64amWoxqfX_horizontal_info_panel');
-    let tabbarVerticalInfoPanel = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical_info_panel');
     tabbarInfoPanel.style.display = 'none';
-    tabbarVerticalInfoPanel.style.display = 'none';
 }
 
 //Close/open pop-up slider script
@@ -128,31 +86,6 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = 'flex';  
     circles[slideIndex-1].className += ' slider_GFBqnm64amWoxqfX_circle_active';
-}
-
-//Position Script
-function verticalPosition() {
-var horizontalHead = document.querySelector('.head_GFBqnm64amWoxqfX');
-    tabbarHorizontal = document.querySelector('.tabbar_GFBqnm64amWoxqfX_horizontal');
-    tabbarUpBtns = document.querySelector('.tabbar_GFBqnm64amWoxqfX_up_btns');
-    tabbarVertical = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical');
-
-    horizontalHead.style.display = 'none';
-    tabbarHorizontal.style.display = 'none';
-    tabbarUpBtns.style.display = 'none';
-    tabbarVertical.style.display = 'flex';
-}
-
-function horizontalPosition() {
-var horizontalHead = document.querySelector('.head_GFBqnm64amWoxqfX');
-    tabbarHorizontal = document.querySelector('.tabbar_GFBqnm64amWoxqfX_horizontal');
-    tabbarUpBtns = document.querySelector('.tabbar_GFBqnm64amWoxqfX_up_btns');
-    tabbarVertical = document.querySelector('.tabbar_GFBqnm64amWoxqfX_vertical');
-
-    horizontalHead.style.display = 'flex';
-    tabbarHorizontal.style.display = 'flex';
-    tabbarUpBtns.style.display = 'flex';
-    tabbarVertical.style.display = 'none';
 }
 
 //AddCart Animation
